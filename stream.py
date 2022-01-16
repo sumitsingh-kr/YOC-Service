17e2044ec245ef55b0b7f879ca83ee21426aaa52
sumitsingh-kr <83880499+sumitsingh-kr@users.noreply.github.com>
2022-01-17 03:01:35 +0530

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,31 @@
+from kafka import KafkaConsumer
+from hdfs import InsecureClient
+import pandas as pd
+import csv
+import json
+import os
+
+from pandas.core.indexes.base import Index
+my_consumer = KafkaConsumer(  
+    'de-capstone3',  
+    bootstrap_servers = ['18.211.252.152 : 9092'],  
+    auto_offset_reset = 'earliest',  
+    enable_auto_commit = True,   
+    value_deserializer = lambda x : (x.decode('utf-8'))  
+    )
+
+data=[]
+
+for i in my_consumer:   
+    if i.value != 'some_message_bytes':
+        data.append(json.loads(i.value))
+    else:
+        break
+
+df = pd.DataFrame(data)
+client_hdfs = InsecureClient('http://localhost:9870')
+
+df['timestamp\n'].replace('\n','',regex=True , inplace=True)
+
+with client_hdfs.write('/yoc_project/clickstream/clickstream.csv', encoding = 'utf-8') as writer:
+    df.to_csv(writer,header=False,index=False)
